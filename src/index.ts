ca3ad013ef9ad19475b7264805096c6db54fff89
BrunoMonteiro25 <bruno.monteiro.silva25@gmail.com>
2022-11-20 23:06:52 -0300

    CORS e Socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,36 @@
 import path from 'node:path'
+import http from 'node:http'
 import express from 'express'
 import mongoose from 'mongoose'
+import { Server } from 'socket.io'
+
 import { router } from './router'
 
-mongoose.connect('mongodb://127.0.0.1:27017/waiterApp')
+const app = express()
+const server = http.createServer(app)
+export const io = new Server(server)
+
+mongoose
+  .connect('mongodb://127.0.0.1:27017/waiterApp')
   .then(() => {
-    const app = express()
     const port = 3001
 
-    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.setHeader('Access-Control-Allow-Methods', '*')
+      res.setHeader('Access-Control-Allow-Headers', '*')
+
+      next()
+    })
+    app.use(
+      '/uploads',
+      express.static(path.resolve(__dirname, '..', 'uploads')),
+    )
     app.use(express.json())
     app.use(router)
 
-    app.listen(3001, () => {
+    server.listen(3001, () => {
       console.log(`Server rodando em http://localhost:${port}`)
     })
   })
   .catch(() => console.log('erro ao conectar no mongodb'))
-
